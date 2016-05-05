de20280b4481456b51a8ebba5a71633885b49255
Ramona Bellamy <ramonabellamy@Raikas-MacBook-Pro-208.local>
2016-05-05 09:56:40 -0500

    new

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+/* view at http://localhost:8080 */
+var port = process.env.PORT || 8000;
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res) {
+	// res.sendFile(path.join(__dirname + '/public/pdf.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
+    
+});
+
+app.listen(port, function(){
+    console.log('Service started on port:' + port);
+});
